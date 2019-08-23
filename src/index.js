2ad0f7be8548e79d469e06cfd210e665c96a6beb
rktldhkd <rktldhkd@naver.com>
2019-08-23 16:33:24 +0900

    Create React Project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//App 클래스르 inde.html의 id가 root태그 그려라.
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
