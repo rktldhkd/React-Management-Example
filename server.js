44e7a810a1711fed0b887eb7ea94ed3c6eda1de6
rktldhkd <rktldhkd@naver.com>
2019-08-27 16:09:30 +0900

    Mysql Database connection setting

--- server.js
+++ server.js
@@ -1,40 +1,36 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.port || 5000
+const mySql = require('mysql');
+const port = process.env.port || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+//db연결에 필요한 정보 정의
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const connection = mySql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});//createConnection-mysql
+//db연결
+connection.connect();
+
 // ()=> == function() 과 같다. 6버전부터 새로 생긴 형식이다.매개함수 지정시 사용.
 app.get('/api/customers', (request, response) => {
-    //배열형태의 데이터를 넣어주기만 하면 알아서 json 형태로 client에 응답한다.
-    response.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '홍길동',
-            'birthday': '991111',
-            'gender': '남자',
-            'job': '무'
-        },
-        {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '청길동',
-            'birthday': '990111',
-            'gender': '남자',
-            'job': '무'
-        },
-        {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': '이선계',
-            'birthday': '110111',
-            'gender': '남자',
-            'job': '무'
-        }
-    ]);//send()
+    // //배열형태의 데이터를 넣어주기만 하면 알아서 json 형태로 client에 응답한다.
+    // response.send();//send()
+    connection.query(
+        "SELECT * FROM customer",
+        (err, rows, fields) => {
+            response.send(rows);
+        }//내부함수 정의. 뒷쪽 =>를 앞쪽에다 function붙이는것으로 대체 가능
+    );//query
 });//get /api/customers
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
